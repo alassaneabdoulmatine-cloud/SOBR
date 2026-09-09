493946b4255c5889e0ce269085ed691f8d3b143a
alassaneabdoulmatine-cloud <alassaneabdoulmatine@gmail.com>
2026-09-09 23:24:34 +0100

    fix defaultcookie samesite none

--- apps/backend/src/auth.ts
+++ apps/backend/src/auth.ts
@@ -31,7 +31,7 @@ export const auth = betterAuth({
 
   advanced: {
     defaultCookieAttributes: {
-      sameSite: 'lax',
+      sameSite: 'none',
       secure: process.env.NODE_ENV === 'production',
     },
     // ipAddress: {
